3e9fc97e9024c2f0cddfc85d18283131563b6a0c
Melanni Gutierrez <gutierrezmelanni99@gmail.com>
2021-03-02 13:20:53 -0600

    Prueba

---
+++ src/Celular.java
@@ -0,0 +1,57 @@
+///*
+ /* To change this license header, choose License Headers in Project Properties.java
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ *
+package Parcial1;
+
+/**
+ *
+ * @author Flor Melanni Infante
+ */
+public class Celular {
+    
+    //contexto
+   //  todo el alcance que puede tener una variable
+    
+   //atributos
+    //sintaxis para crear variables
+    //modificador de acceso + tipo + nombre
+    
+    //variables locales
+    private String marca;
+    private String capacidad;
+    private String tamaño;
+    private String color;
+    private double precio;
+    private String modelo;
+    private boolean chino;
+    
+    
+    //metodos
+    //metodo especial (constructor)
+    //sintaxis creacion constructor
+    //modificadorPublicoNombre*Clase
+    
+    
+    //metodos o funciones
+    //sintaxis creacion de metodos 
+    //modaccess tipo Nombre
+    
+    
+    //metodos o funciones
+    //sintaxis creacion de metodos
+    //modaccess tipoRetorno Nombre()
+    //void =>vacio
+    //modaccess tipoRetorno Nombre(tipoDato)
+    //parametros =>variable locales
+    
+    public void Llamr (int numero){
+    System.out.println("Marcando al numero:" + numero + "...");
+    }
+    
+    public String ObtenerMarca(){
+    return marca; 
+    }
+}
+ 
---
+++ src/Parcial1/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Parcial1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+ /*
+ * //@author Flor Melanni Infante
+ */
+public class Main {
+    //psvm 
+    public static void main(String[] args) {
+        BufferedReader bf = new BufferedReader
+        //sout
+        System.out.println("Hola mundo!");
+    }
+  }  
+
